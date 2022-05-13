d9007049e4fe7501d70975d2399c127642e703b4
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-13 10:55:12 -0300

    feat: creates images filter

---
+++ src/Components/ImageFilter.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Icone } from "./UI/index";
+
+import alimentacao from "../assets/images/alimentacao.svg";
+import outros from "../assets/images/outros.svg";
+import saude from "../assets/images/saude.svg";
+import transporte from "../assets/images/transporte.svg";
+import utilidades from "../assets/images/utilidades.svg";
+
+export default (type) => {
+  const Images = {
+    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
+    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
+    Saude: <Icone src={saude} alt="Saude" />,
+    Transporte: <Icone src={transporte} alt="Transporte" />,
+    default: <Icone src={outros} alt="Outro" />,
+  };
+
+  return Images[type] || Images.default;
+};
