aff84b5e8ffa241da854818f333ee5ab4fafd625
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-13 14:42:47 -0300

    feat: creates app themes

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,13 @@
+import {fundoClaro, conteudoClaro, textoFundoClaro, fundoEscuro, conteudoEscuro, textFundoEscuro} from './variaveis'
+
+export const temaClaro = {
+  body: fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoClaro
+}
+
+export const temaEscuro = {
+  body: fundoEscuro,
+  inside: conteudoEscuro,
+  text:  textFundoEscuro
+}
