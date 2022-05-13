669ac35a2cba26c10d1820626e4f7eecda48d411
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-13 09:12:58 -0300

    feat: creates button style using styled components

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
- 
+
 import { corPrimaria } from "../UI/variaveis";
 
 import styled from "styled-components";
@@ -12,23 +12,32 @@ const StyledHeader = styled.nav`
   padding: 0 15vw;
   height: 10vh;
   align-items: center;
-`
+`;
 const Logo = styled.img`
   height: 50px;
   width: 50px;
-`
+`;
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props) => (props.primary ? "white" : corPrimaria)};
+  color: ${(props) => (props.primary ? corPrimaria : "white")};
+`;
 
 const Cabecalho = () => {
   return (
     <StyledHeader>
       <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnCabecalho primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
+        </BtnCabecalho>
+        <BtnCabecalho href="https://google.com">Sair</BtnCabecalho>
       </div>
     </StyledHeader>
   );
