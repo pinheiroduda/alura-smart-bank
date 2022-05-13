2b882df336706bf651361982a7571c9e579e152b
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-13 14:50:12 -0300

    feat: adds styled components theme provider

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import {GlobalStyle} from './Components/GlobalStyle'
 
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
+
+import {ThemeProvider} from 'styled-components'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaEscuro}>
       <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
