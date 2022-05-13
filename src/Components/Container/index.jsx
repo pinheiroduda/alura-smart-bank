0aa915e9c8726099f7032254780364b62b60b142
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-13 14:51:07 -0300

    feat: adds style dinamically according to chosen theme

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -7,7 +7,7 @@ import styled from "styled-components";
 import { Extrato } from "../Extrato";
 
 const Container = styled.div`
-  background-color: #f1f1f1;
+  background-color: ${({theme}) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
 `;
@@ -16,6 +16,7 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  color:  ${({theme}) => theme.text};
 
   @media (max-width: 800px) {
     flex-direction: column;
