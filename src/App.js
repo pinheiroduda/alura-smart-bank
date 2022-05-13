3959202a90de124165a290c6c227739a6395b587
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-05-13 15:12:21 -0300

    feat: creates a function to switch theme when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import {GlobalStyle} from './Components/GlobalStyle'
-
+import { BtnTema } from "./Components/UI";
 import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import {ThemeProvider} from 'styled-components'
+import SwitcherTema from "./Components/SwitcherTema";
 
 function App() {
+  const [tema, setTema] = useState(true)
+
+  const toggleTema = () => {
+    setTema((tema) => !tema)
+  }
+
   return (
-    <ThemeProvider theme={temaEscuro}>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
